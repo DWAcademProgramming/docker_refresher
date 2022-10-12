f15c7820e88a3e992197db7585ac4133f4ec404a
gntpldes <h3c6j6@u.northwestern.edu>
2022-10-12 13:41:47 -0500

    Error Routes Added: Mongo Added

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,23 +1,28 @@
-
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
+
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
-import { signUpRouter } from './routes/signup';
 import { signOutRouter } from './routes/signout';
+import { signUpRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
- 
+import { NotFoundError } from './errors/not-found-error';
+
 const app = express();
- 
 app.use(json());
- 
+
 app.use(currentUserRouter);
 app.use(signinRouter);
-app.use(signUpRouter);
 app.use(signOutRouter);
+app.use(signUpRouter);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
 
 app.use(errorHandler);
- 
+
 app.listen(3000, () => {
-    console.log('Auth service listening')
+  console.log('Listening on port 3000!!!!!!!!');
 });
--- ticketing/auth/src/middlewares/error-handler.ts
+++ ticketing/auth/src/middlewares/error-handler.ts
@@ -1,13 +1,17 @@
-import { Request, Response, NextFunction } from "express"
+import { Request, Response, NextFunction } from "express";
+import { CustomError } from "../errors/custom-error";
 
 export const errorHandler = (
-    err: Error, 
-    req: Request, 
-    res: Response, 
-    next: NextFunction) =>{
-    console.log('Something went wrong', err); 
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
 
-    res.status(400).send({
-        message: 'something went wrong'
-    });
-}; 
+  res.status(400).send({
+    errors: [{ message: "Something went wrong" }],
+  });
+};
