a7032f4d3775de3b610323ee6408e61f77bb4482
gntpldes <h3c6j6@u.northwestern.edu>
2022-10-12 06:47:24 -0500

    Everything seems to be working...
    Seems is the key word...

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,18 +1,20 @@
-import express from "express";
-import { json } from 'body-parser'; 
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signOutRouter } from "./routes/signout";
-import { signUpRouter } from "./routes/signup";
 
-const app = express(); 
-app.use(json()); 
-
-app.use(currentUserRouter); 
-app.use(signinRouter); 
-app.use(signOutRouter); 
-app.use(signUpRouter); 
-
-app.listen(3000, () =>{
-    console.log('Listening on port 3000!!!!'); 
-}); 
+import express from 'express'
+import { json } from 'body-parser'
+import { currentUserRouter } from './routes/current-user'
+import { signinRouter } from './routes/signin'
+import { signUpRouter } from './routes/signup'
+import { signOutRouter } from './routes/signout'
+ 
+const app = express()
+ 
+app.use(json())
+ 
+app.use(currentUserRouter)
+app.use(signinRouter)
+app.use(signUpRouter)
+app.use(signOutRouter)
+ 
+app.listen(3000, () => {
+    console.log('Auth service listening')
+});
